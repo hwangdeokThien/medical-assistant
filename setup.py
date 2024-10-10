12ad6a7505b9dc6e231959143a5cec12b45df0de
hwangdeokthien <huynhducthien41906@gmail.com>
2024-10-10 10:49:27 +0700

    [ADD] setup config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Assistant",
+    version="0.0.0",
+    author='Duc Thien',
+    author_email='huynhducthien41906@gmail',
+    packages=find_packages(),
+    install_requires=[]
+)
